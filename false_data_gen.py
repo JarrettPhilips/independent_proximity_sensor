2faeec4898bf16ea59345ef7c2a346c0b908bd65
Jarrett Philips <jarrettphilips@gmail.com>
2020-04-29 03:06:21 -0600

    added multithreading to wait for TCP and make plot interactive at once

--- false_data_gen.py
+++ false_data_gen.py
@@ -4,18 +4,26 @@
 	File for generating fake data to test the functionality of the ips monitor
 '''
 
+import random
 import json
 import socket
-
+import time
 
 TCP_IP = '127.0.0.1'
 TCP_PORT = 5005
 BUFFER_SIZE = 1024
-MESSAGE = '1,1,1'
+#MESSAGE = '-1,-10,1'
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((TCP_IP, TCP_PORT))
-s.send(MESSAGE.encode())
-s.close()
+while 1 :
+	x = random.randint(-100, 100)
+	y = random.randint(-100, 100)
+	z = random.randint(-100, 100)
+	MESSAGE = str(x) + ',' + str(y) + ',' + str(z)
 
-print("sent:", MESSAGE)
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.connect((TCP_IP, TCP_PORT))
+	s.send(MESSAGE.encode())
+	s.close()
+
+	print("sent:", MESSAGE)
+	time.sleep(2)
--- ips_monitor.py
+++ ips_monitor.py
@@ -9,6 +9,7 @@ import logger
 import time
 import socket
 import json
+import threading
 
 import numpy as np
 from matplotlib import pyplot as plt
@@ -22,6 +23,10 @@ BUFFER_SIZE = 20  #Default is 1024
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((TCP_IP, TCP_PORT))
+
+#Global variable for multithreading thing
+data_string_thread_out = None
+data_string_thread_out_last = None
 
 #Setup for data logger
 path = "/Users/jarrettphilips/Desktop/ips_logs"
@@ -45,15 +50,20 @@ plt.pause(0.0001)
 
 #####################################
 def wait_for_data():
-	s.listen(1)
-	conn, addr = s.accept()
-	print('Connection address:', addr)
+	while 1 :
+		s.listen(1)
+		conn, addr = s.accept()
+		print('Connection address:', addr)
 
-	while 1:
-		data_string = conn.recv(BUFFER_SIZE)
-		if not data_string: break
-		print("received data:", data_string)
-		return data_string.decode()
+		while 1:
+			data_string = conn.recv(BUFFER_SIZE)
+			if not data_string: break
+			print("received data:", data_string)
+
+			global data_string_thread_out
+			data_string_thread_out = data_string.decode()
+			#return data_string.decode()
+	conn.close()
 
 def parse_data(data_string):
 	data_string_split = data_string.split(',')
@@ -67,16 +77,26 @@ def plot_point(x, y, z):
 	global graph
 	graph = ax.scatter([x], [y], [z], color='red')
 	plt.show(block=False)
-	plt.pause(0.0001)
+	#plt.pause(0.0001)
+	fig.canvas.start_event_loop(0.001)
 
 #####################################
 def main():
+	t = threading.Thread(target=wait_for_data, args=())
+	t.start()
 	while 1: 
-		data_string = wait_for_data()
-		(x, y, z) = parse_data(data_string)
+		#data_string = wait_for_data()
+		#(x, y, z) = parse_data(data_string)
 
-		log_point(x, y, z)
-		plot_point(x, y, z)
+		#plt.pause(.1)
+		fig.canvas.start_event_loop(0.001)
+		global data_string_thread_out_last
+		if (data_string_thread_out != None) and (data_string_thread_out != data_string_thread_out_last):
+			data_string_thread_out_last = data_string_thread_out
+			(x, y, z) = parse_data(data_string_thread_out)
+
+			log_point(x, y, z)
+			plot_point(x, y, z)
 
 main()
 log.close_log()
